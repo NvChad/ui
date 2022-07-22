c47bf643dbdfef37c31097bd6209463bb2e816ac
siduck <siduck@tutanota.com>
2022-07-22 16:01:39 +0000

    format files

--- lua/nvchad_ui/icons.lua
+++ lua/nvchad_ui/icons.lua
@@ -1,207 +1,207 @@
 local M = {}
 
 M.lspkind = {
-   Namespace = "",
-   Text = " ",
-   Method = " ",
-   Function = " ",
-   Constructor = " ",
-   Field = "ﰠ ",
-   Variable = " ",
-   Class = "ﴯ ",
-   Interface = " ",
-   Module = " ",
-   Property = "ﰠ ",
-   Unit = "塞 ",
-   Value = " ",
-   Enum = " ",
-   Keyword = " ",
-   Snippet = " ",
-   Color = " ",
-   File = " ",
-   Reference = " ",
-   Folder = " ",
-   EnumMember = " ",
-   Constant = " ",
-   Struct = "פּ ",
-   Event = " ",
-   Operator = " ",
-   TypeParameter = " ",
-   Table = "",
-   Object = " ",
-   Tag = "",
-   Array = "[]",
-   Boolean = " ",
-   Number = " ",
-   Null = "ﳠ",
-   String = " ",
-   Calendar = "",
-   Watch = " ",
-   Package = "",
+  Namespace = "",
+  Text = " ",
+  Method = " ",
+  Function = " ",
+  Constructor = " ",
+  Field = "ﰠ ",
+  Variable = " ",
+  Class = "ﴯ ",
+  Interface = " ",
+  Module = " ",
+  Property = "ﰠ ",
+  Unit = "塞 ",
+  Value = " ",
+  Enum = " ",
+  Keyword = " ",
+  Snippet = " ",
+  Color = " ",
+  File = " ",
+  Reference = " ",
+  Folder = " ",
+  EnumMember = " ",
+  Constant = " ",
+  Struct = "פּ ",
+  Event = " ",
+  Operator = " ",
+  TypeParameter = " ",
+  Table = "",
+  Object = " ",
+  Tag = "",
+  Array = "[]",
+  Boolean = " ",
+  Number = " ",
+  Null = "ﳠ",
+  String = " ",
+  Calendar = "",
+  Watch = " ",
+  Package = "",
 }
 
 M.statusline_separators = {
-   default = {
-      left = "",
-      right = " ",
-   },
+  default = {
+    left = "",
+    right = " ",
+  },
 
-   round = {
-      left = "",
-      right = "",
-   },
+  round = {
+    left = "",
+    right = "",
+  },
 
-   block = {
-      left = "█",
-      right = "█",
-   },
+  block = {
+    left = "█",
+    right = "█",
+  },
 
-   arrow = {
-      left = "",
-      right = "",
-   },
+  arrow = {
+    left = "",
+    right = "",
+  },
 }
 
 M.devicons = {
-   default_icon = {
-      icon = "",
-      name = "Default",
-   },
+  default_icon = {
+    icon = "",
+    name = "Default",
+  },
 
-   c = {
-      icon = "",
-      name = "c",
-   },
+  c = {
+    icon = "",
+    name = "c",
+  },
 
-   css = {
-      icon = "",
-      name = "css",
-   },
+  css = {
+    icon = "",
+    name = "css",
+  },
 
-   deb = {
-      icon = "",
-      name = "deb",
-   },
+  deb = {
+    icon = "",
+    name = "deb",
+  },
 
-   Dockerfile = {
-      icon = "",
-      name = "Dockerfile",
-   },
+  Dockerfile = {
+    icon = "",
+    name = "Dockerfile",
+  },
 
-   html = {
-      icon = "",
-      name = "html",
-   },
+  html = {
+    icon = "",
+    name = "html",
+  },
 
-   jpeg = {
-      icon = "",
-      name = "jpeg",
-   },
+  jpeg = {
+    icon = "",
+    name = "jpeg",
+  },
 
-   jpg = {
-      icon = "",
-      name = "jpg",
-   },
+  jpg = {
+    icon = "",
+    name = "jpg",
+  },
 
-   js = {
-      icon = "",
-      name = "js",
-   },
+  js = {
+    icon = "",
+    name = "js",
+  },
 
-   kt = {
-      icon = "󱈙",
-      name = "kt",
-   },
+  kt = {
+    icon = "󱈙",
+    name = "kt",
+  },
 
-   lock = {
-      icon = "",
-      name = "lock",
-   },
+  lock = {
+    icon = "",
+    name = "lock",
+  },
 
-   lua = {
-      icon = "",
-      name = "lua",
-   },
+  lua = {
+    icon = "",
+    name = "lua",
+  },
 
-   mp3 = {
-      icon = "",
-      name = "mp3",
-   },
+  mp3 = {
+    icon = "",
+    name = "mp3",
+  },
 
-   mp4 = {
-      icon = "",
-      name = "mp4",
-   },
+  mp4 = {
+    icon = "",
+    name = "mp4",
+  },
 
-   out = {
-      icon = "",
-      name = "out",
-   },
+  out = {
+    icon = "",
+    name = "out",
+  },
 
-   png = {
-      icon = "",
-      name = "png",
-   },
+  png = {
+    icon = "",
+    name = "png",
+  },
 
-   py = {
-      icon = "",
-      name = "py",
-   },
+  py = {
+    icon = "",
+    name = "py",
+  },
 
-   ["robots.txt"] = {
-      icon = "ﮧ",
-      name = "robots",
-   },
+  ["robots.txt"] = {
+    icon = "ﮧ",
+    name = "robots",
+  },
 
-   toml = {
-      icon = "",
-      name = "toml",
-   },
+  toml = {
+    icon = "",
+    name = "toml",
+  },
 
-   ts = {
-      icon = "ﯤ",
-      name = "ts",
-   },
+  ts = {
+    icon = "ﯤ",
+    name = "ts",
+  },
 
-   ttf = {
-      icon = "",
-      name = "TrueTypeFont",
-   },
+  ttf = {
+    icon = "",
+    name = "TrueTypeFont",
+  },
 
-   rb = {
-      icon = "",
-      name = "rb",
-   },
+  rb = {
+    icon = "",
+    name = "rb",
+  },
 
-   rpm = {
-      icon = "",
-      name = "rpm",
-   },
+  rpm = {
+    icon = "",
+    name = "rpm",
+  },
 
-   vue = {
-      icon = "﵂",
-      name = "vue",
-   },
+  vue = {
+    icon = "﵂",
+    name = "vue",
+  },
 
-   woff = {
-      icon = "",
-      name = "WebOpenFontFormat",
-   },
+  woff = {
+    icon = "",
+    name = "WebOpenFontFormat",
+  },
 
-   woff2 = {
-      icon = "",
-      name = "WebOpenFontFormat2",
-   },
+  woff2 = {
+    icon = "",
+    name = "WebOpenFontFormat2",
+  },
 
-   xz = {
-      icon = "",
-      name = "xz",
-   },
+  xz = {
+    icon = "",
+    name = "xz",
+  },
 
-   zip = {
-      icon = "",
-      name = "zip",
-   },
+  zip = {
+    icon = "",
+    name = "zip",
+  },
 }
 
 return M
--- lua/nvchad_ui/tabufline/init.lua
+++ lua/nvchad_ui/tabufline/init.lua
@@ -1,17 +1,17 @@
 local tabufline_opts = require("nvchad_ui").tabufline
 
 return {
-   setup = function(opts)
-      tabufline_opts = vim.tbl_deep_extend("force", tabufline_opts, opts.tabufline or {})
+  setup = function(opts)
+    tabufline_opts = vim.tbl_deep_extend("force", tabufline_opts, opts.tabufline or {})
 
-      local modules = require "nvchad_ui.tabufline.modules"
+    local modules = require "nvchad_ui.tabufline.modules"
 
-      -- merge user modules :D
-      if tabufline_opts.overriden_modules then
-         modules = vim.tbl_deep_extend("force", modules, tabufline_opts.overriden_modules())
-      end
+    -- merge user modules :D
+    if tabufline_opts.overriden_modules then
+      modules = vim.tbl_deep_extend("force", modules, tabufline_opts.overriden_modules())
+    end
 
-      local result = modules.bufferlist() .. (modules.tablist() or "") .. modules.buttons()
-      return (vim.g.nvimtree_side == "left" ) and modules.CoverNvimTree() .. result or result .. modules.CoverNvimTree()
-   end,
+    local result = modules.bufferlist() .. (modules.tablist() or "") .. modules.buttons()
+    return (vim.g.nvimtree_side == "left") and modules.CoverNvimTree() .. result or result .. modules.CoverNvimTree()
+  end,
 }
