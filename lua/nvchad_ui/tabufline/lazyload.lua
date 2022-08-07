bca1182e872db0ea52d86b2f4cf4ee7cb092de45
siduck <siduck@tutanota.com>
2022-08-07 08:14:59 +0530

    load tabufline mappings only if its enabled

--- lua/nvchad_ui/tabufline/lazyload.lua
+++ lua/nvchad_ui/tabufline/lazyload.lua
@@ -1,5 +1,11 @@
 return function(opts)
-  if opts.enabled and opts.lazyload then
+  if not opts.enabled then
+    return
+  end
+
+  require("core.utils").load_mappings "tabufline"
+
+  if opts.lazyload then
     vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead", "TabEnter" }, {
       pattern = "*",
       group = vim.api.nvim_create_augroup("TabuflineLazyLoad", {}),
@@ -11,7 +17,7 @@ return function(opts)
         end
       end,
     })
-  elseif opts.enabled then
+  else
     vim.opt.showtabline = 2
     vim.opt.tabline = "%!v:lua.require('nvchad_ui').tabufline()"
   end
