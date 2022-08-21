aa836e674231424bcf5929894866380d8bcc7b30
Akianonymus <anonymus.aki@gmail.com>
2022-08-21 17:35:31 +0530

    tabufline: Move relavent code from main repo to here

--- lua/nvchad_ui/tabufline/lazyload.lua
+++ lua/nvchad_ui/tabufline/lazyload.lua
@@ -3,6 +3,44 @@ return function(opts)
     return
   end
 
+  -- store listed buffers in tab local var
+  vim.t.bufs = vim.api.nvim_list_bufs()
+
+  -- autocmds for tabufline -> store bufnrs on bufadd, bufenter events
+  -- thx to https://github.com/ii14 & stores buffer per tab -> table
+  vim.api.nvim_create_autocmd({ "BufAdd", "BufEnter" }, {
+    callback = function(args)
+      if vim.t.bufs == nil then
+        vim.t.bufs = { args.buf }
+      else
+        local bufs = vim.t.bufs
+
+        -- check for duplicates
+        if not vim.tbl_contains(bufs, args.buf) and (args.event == "BufAdd" or vim.bo[args.buf].buflisted) then
+          table.insert(bufs, args.buf)
+          vim.t.bufs = bufs
+        end
+      end
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("BufDelete", {
+    callback = function(args)
+      for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
+        local bufs = vim.t[tab].bufs
+        if bufs then
+          for i, bufnr in ipairs(bufs) do
+            if bufnr == args.buf then
+              table.remove(bufs, i)
+              vim.t[tab].bufs = bufs
+              break
+            end
+          end
+        end
+      end
+    end,
+  })
+
   require("core.utils").load_mappings "tabufline"
 
   if opts.lazyload then
