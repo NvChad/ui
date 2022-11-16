NvChad/ui

commit 0bc144923e596d53f7635d889858871e2f8ecb59

Fix(tabufline): fixed incorrect buf validity check, fixed adding current buf to new tab  (#30)
* Fix(tabufline): fixed incorrect buf validity check

Combine `vim.api.nvim_buf_is_valid` with `vim.bo[bufnr].buflisted` to verify the validity of buf. Fix #22

* Fix(tabufline): fixed adding current buf to new tab

Current behavior: When a new tab is created, the current buf is also added to it.
Fix: add `event ~= BufAdd` to prevent adding current buf.

* Fix(tabufline): fixed adding current buf to new tab

To reproduce: open any file, PackerCompile, then tabnew.

* Fix(tabufline): fixed adding current buf to new tab, again.

To reproduce: open any file, run checkhealth.

* Delete utils.lua

* Fix(tabufline): move the isBufValid function to init.lua

lua/nvchad_ui/tabufline/lazyload.lua
@@ -11,12 +11,17 @@ return function(opts)
   vim.api.nvim_create_autocmd({ "BufAdd", "BufEnter", "tabnew" }, {
     callback = function(args)
       if vim.t.bufs == nil then
-        vim.t.bufs = { args.buf }
+        vim.t.bufs = vim.api.nvim_get_current_buf() == args.buf and {} or { args.buf }
       else
         local bufs = vim.t.bufs
 
         -- check for duplicates
-        if not vim.tbl_contains(bufs, args.buf) and (args.event == "BufAdd" or vim.bo[args.buf].buflisted) then
+        if
+          not vim.tbl_contains(bufs, args.buf)
+          and (args.event == "BufEnter" or vim.bo[args.buf].buflisted)
+          and (args.event == "BufEnter" or args.buf ~= vim.api.nvim_get_current_buf())
+          and args.event ~= "BufAdd"
+        then
           table.insert(bufs, args.buf)
           vim.t.bufs = bufs
         end

lua/nvchad_ui/tabufline/init.lua
@@ -1,6 +1,10 @@
 local M = {}
 local api = vim.api
 
+M.isBufValid = function(bufnr)
+  return vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].buflisted
+end
+
 M.bufilter = function()
   local bufs = vim.t.bufs or nil
 
@@ -9,7 +13,7 @@ M.bufilter = function()
   end
 
   for i = #bufs, 1, -1 do
-    if not api.nvim_buf_is_valid(bufs[i]) then
+    if not M.isBufValid(bufs[i]) then
       table.remove(bufs, i)
     end
   end

lua/nvchad_ui/tabufline/modules.lua
@@ -2,6 +2,7 @@ local api = vim.api
 local devicons_present, devicons = pcall(require, "nvim-web-devicons")
 local fn = vim.fn
 local new_cmd = api.nvim_create_user_command
+local isBufValid = require("nvchad_ui.tabufline").isBufValid
 
 require("base46").load_highlight "tbline"
 
@@ -97,7 +98,7 @@ local function add_fileInfo(name, bufnr)
 
     -- check for same buffer names under different dirs
     for _, value in ipairs(vim.t.bufs) do
-      if api.nvim_buf_is_valid(value) then
+      if isBufValid(value) then
         if name == fn.fnamemodify(api.nvim_buf_get_name(value), ":t") and value ~= bufnr then
           local other = {}
           for match in (api.nvim_buf_get_name(value) .. "/"):gmatch("(.-)" .. "/") do
@@ -175,7 +176,7 @@ M.bufferlist = function()
 
   vim.g.bufirst = 0
   for _, bufnr in ipairs(vim.t.bufs) do
-    if api.nvim_buf_is_valid(bufnr) then
+    if isBufValid(bufnr) then
       if ((#buffers + 1) * 21) > available_space then
         if has_current then
           break

commit 6466e8e70a3164c5ddf4739b169dcca10da6b6b5

Fix(tabufline): Removing wrong bufs.
The bug introduced by #30 described by #37  , except for the bug described by #37 , there is a problem that the BUF does not display before fixing. Repair now, please review. @siduck @charludo @jtuz

lua/nvchad_ui/tabufline/lazyload.lua
@@ -21,7 +21,6 @@ return function(opts)
           not vim.tbl_contains(bufs, args.buf)
           and (args.event == "BufEnter" or vim.bo[args.buf].buflisted)
           and (args.event == "BufEnter" or args.buf ~= vim.api.nvim_get_current_buf())
-          and args.event ~= "BufAdd"
           and isBufValid(args.buf)
         then
           table.insert(bufs, args.buf)