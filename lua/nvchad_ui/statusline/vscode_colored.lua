0667ee1192ff012c48da910d38131927d8e03201
siduck <siduck@tutanota.com>
2022-11-26 13:17:46 +0530

    add vscode_colored statusline theme

--- lua/nvchad_ui/statusline/vscode_colored.lua
+++ lua/nvchad_ui/statusline/vscode_colored.lua
@@ -1,4 +1,4 @@
-loadfile(vim.g.base46_cache .. "vscode_st")()
+loadfile(vim.g.base46_cache .. "vscode_colored_st")()
 
 local fn = vim.fn
 local config = require("core.utils").load_config().ui.statusline
@@ -6,38 +6,38 @@ local config = require("core.utils").load_config().ui.statusline
 local M = {}
 
 M.modes = {
-  ["n"] = "NORMAL",
-  ["niI"] = "NORMAL i",
-  ["niR"] = "NORMAL r",
-  ["niV"] = "NORMAL v",
-  ["no"] = "N-PENDING",
-  ["i"] = "INSERT",
-  ["ic"] = "INSERT (completion)",
-  ["ix"] = "INSERT completion",
-  ["t"] = "TERMINAL",
-  ["nt"] = "NTERMINAL",
-  ["v"] = "VISUAL",
-  ["V"] = "V-LINE",
-  ["Vs"] = "V-LINE (Ctrl O)",
-  [""] = "V-BLOCK",
-  ["R"] = "REPLACE",
-  ["Rv"] = "V-REPLACE",
-  ["s"] = "SELECT",
-  ["S"] = "S-LINE",
-  [""] = "S-BLOCK",
-  ["c"] = "COMMAND",
-  ["cv"] = "COMMAND",
-  ["ce"] = "COMMAND",
-  ["r"] = "PROMPT",
-  ["rm"] = "MORE",
-  ["r?"] = "CONFIRM",
-  ["x"] = "CONFIRM",
-  ["!"] = "SHELL",
+  ["n"] = { "NORMAL", "St_NormalMode" },
+  ["niI"] = { "NORMAL i", "St_NormalMode" },
+  ["niR"] = { "NORMAL r", "St_NormalMode" },
+  ["niV"] = { "NORMAL v", "St_NormalMode" },
+  ["no"] = { "N-PENDING", "St_NormalMode" },
+  ["i"] = { "INSERT", "St_InsertMode" },
+  ["ic"] = { "INSERT (completion)", "St_InsertMode" },
+  ["ix"] = { "INSERT completion", "St_InsertMode" },
+  ["t"] = { "TERMINAL", "St_TerminalMode" },
+  ["nt"] = { "NTERMINAL", "St_NTerminalMode" },
+  ["v"] = { "VISUAL", "St_VisualMode" },
+  ["V"] = { "V-LINE", "St_VisualMode" },
+  ["Vs"] = { "V-LINE (Ctrl O)", "St_VisualMode" },
+  [""] = { "V-BLOCK", "St_VisualMode" },
+  ["R"] = { "REPLACE", "St_ReplaceMode" },
+  ["Rv"] = { "V-REPLACE", "St_ReplaceMode" },
+  ["s"] = { "SELECT", "St_SelectMode" },
+  ["S"] = { "S-LINE", "St_SelectMode" },
+  [""] = { "S-BLOCK", "St_SelectMode" },
+  ["c"] = { "COMMAND", "St_CommandMode" },
+  ["cv"] = { "COMMAND", "St_CommandMode" },
+  ["ce"] = { "COMMAND", "St_CommandMode" },
+  ["r"] = { "PROMPT", "St_ConfirmMode" },
+  ["rm"] = { "MORE", "St_ConfirmMode" },
+  ["r?"] = { "CONFIRM", "St_ConfirmMode" },
+  ["x"] = { "CONFIRM", "St_ConfirmMode" },
+  ["!"] = { "SHELL", "St_TerminalMode" },
 }
 
 M.mode = function()
   local m = vim.api.nvim_get_mode().mode
-  return "%#St_Mode#" .. string.format("  %s ", M.modes[m])
+  return "%#" .. M.modes[m][2] .. "#" .. "  " .. M.modes[m][1] .. " "
 end
 
 M.fileInfo = function()
@@ -76,9 +76,11 @@ M.gitchanges = function()
 
   local git_status = vim.b.gitsigns_status_dict
 
-  local added = (git_status.added and git_status.added ~= 0) and ("  " .. git_status.added) or ""
-  local changed = (git_status.changed and git_status.changed ~= 0) and ("  " .. git_status.changed) or ""
-  local removed = (git_status.removed and git_status.removed ~= 0) and ("  " .. git_status.removed) or ""
+  local added = (git_status.added and git_status.added ~= 0) and ("%#St_lspInfo#  " .. git_status.added .. " ") or ""
+  local changed = (git_status.changed and git_status.changed ~= 0) and ("%#St_lspWarning#  " .. git_status.changed .. " ")
+    or ""
+  local removed = (git_status.removed and git_status.removed ~= 0) and ("%#St_lspError#  " .. git_status.removed .. " ")
+    or ""
 
   return (added .. changed .. removed) ~= "" and (added .. changed .. removed .. " | ") or ""
 end
@@ -103,12 +105,12 @@ M.LSP_progress = function()
   local frame = math.floor(ms / 120) % #spinners
   local content = string.format(" %%<%s %s %s (%s%%%%) ", spinners[frame + 1], title, msg, percentage)
 
-  return content or ""
+  return ("%#St_LspProgress#" .. content) or ""
 end
 
 M.LSP_Diagnostics = function()
   if not rawget(vim, "lsp") then
-    return "  0  0"
+    return "%#St_lspError#  0 %#St_lspWarning# 0"
   end
 
   local errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
@@ -116,35 +118,35 @@ M.LSP_Diagnostics = function()
   local hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
   local info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
 
-  errors = (errors and errors > 0) and (" " .. errors .. " ") or " 0 "
-  warnings = (warnings and warnings > 0) and (" " .. warnings .. " ") or " 0 "
-  hints = (hints and hints > 0) and ("ﯧ " .. hints .. " ") or ""
-  info = (info and info > 0) and (" " .. info .. " ") or ""
+  errors = (errors and errors > 0) and ("%#St_lspError# " .. errors .. " ") or "%#St_lspError# 0 "
+  warnings = (warnings and warnings > 0) and ("%#St_lspWarning# " .. warnings .. " ") or "%#St_lspWarning# 0 "
+  hints = (hints and hints > 0) and ("%#St_lspHints#ﯧ " .. hints .. " ") or ""
+  info = (info and info > 0) and ("%#St_lspInfo# " .. info .. " ") or ""
 
   return vim.o.columns > 140 and errors .. warnings .. hints .. info or ""
 end
 
 M.filetype = function()
-  return vim.bo.ft == "" and "{} plain text  " or "{} " .. vim.bo.ft .. " "
+  return vim.bo.ft == "" and "%#St_ft# {} plain text  " or "%#St_ft#{} " .. vim.bo.ft .. " "
 end
 
 M.LSP_status = function()
   if rawget(vim, "lsp") then
     for _, client in ipairs(vim.lsp.get_active_clients()) do
       if client.attached_buffers[vim.api.nvim_get_current_buf()] then
-        return (vim.o.columns > 100 and "   " .. client.name .. "  ") or "   LSP  "
+        return (vim.o.columns > 100 and "%#St_LspStatus#   " .. client.name .. "  ") or "   LSP  "
       end
     end
   end
 end
 
 M.cwd = function()
-  local dir_name = "%#St_Mode#  " .. fn.fnamemodify(fn.getcwd(), ":t") .. " "
+  local dir_name = "%#St_cwd#  " .. fn.fnamemodify(fn.getcwd(), ":t") .. " "
   return (vim.o.columns > 85 and dir_name) or ""
 end
 
 M.run = function()
-  local modules = require "nvchad_ui.statusline.vscode"
+  local modules = require "nvchad_ui.statusline.vscode_colored"
 
   if config.overriden_modules then
     modules = vim.tbl_deep_extend("force", modules, config.overriden_modules())
@@ -161,8 +163,8 @@ M.run = function()
     "%=",
 
     modules.gitchanges(),
-    vim.o.columns > 140 and "Ln %l, Col %c  " or "",
-    string.upper(vim.bo.fileencoding) == "" and "" or string.upper(vim.bo.fileencoding) .. "  ",
+    vim.o.columns > 140 and "%#StText# Ln %l, Col %c  " or "",
+    string.upper(vim.bo.fileencoding) == "" and "" or "%#St_encode#" .. string.upper(vim.bo.fileencoding) .. "  ",
     modules.filetype(),
     modules.LSP_status() or "",
     modules.cwd(),
