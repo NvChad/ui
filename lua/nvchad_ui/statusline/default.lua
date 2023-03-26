a3fd7134c43384f8b992c713ad6e2a60dc547a97
Nikita Rudenko <rudenkornk@gmail.com>
2023-03-26 08:57:07 +0300

    Equalize length of the cursor_position block in default style
    When cursor changes its position from 0th line to the 1st, this block
    transforms from "Top" to "0%", which has one characrer less.
    The whole right block is twitching because of this.

--- lua/nvchad_ui/statusline/default.lua
+++ lua/nvchad_ui/statusline/default.lua
@@ -154,6 +154,7 @@ M.cursor_position = function()
   local current_line = fn.line "."
   local total_line = fn.line "$"
   local text = math.modf((current_line / total_line) * 100) .. tostring "%%"
+  text = string.format("%4s", text)
 
   text = (current_line == 1 and "Top") or text
   text = (current_line == total_line and "Bot") or text
