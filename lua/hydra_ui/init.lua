85ad6a8010891f221b39c2e4d1ac5b5d7d7ea363
Diogo Silva <ssdiogo@proton.me>
2023-04-27 09:37:57 -0300

    added: reponsive dash

--- lua/hydra_ui/init.lua
+++ lua/hydra_ui/init.lua
@@ -14,3 +14,11 @@ if vim.g.hydravim_dash then
         require("hydra_ui.dash").open()
     end, 0)
 end
+
+vim.api.nvim_create_autocmd("VimResized", {
+    callback = function()
+        if vim.bo.filetype == "dash" then
+            vim.cmd "HydraVimDash"
+        end
+    end,
+})
