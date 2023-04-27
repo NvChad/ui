bfbcb75dd52933a1772ce38bd0fb6b4068f881ca
Diogo Silva <ssdiogo@proton.me>
2023-04-27 09:29:01 -0300

    Update init.lua

--- lua/hydra_ui/init.lua
+++ lua/hydra_ui/init.lua
@@ -1,14 +1,16 @@
 local new_cmd = vim.api.nvim_create_user_command
-new_cmd("HidraDash", function()
-  if vim.g.nvdash_displayed then
-    vim.cmd "bd"
-  else
-    require("hydra_ui.dash").open(vim.api.nvim_create_buf(false, true))
-  end
-end, {})
+
+new_cmd("HydraVimDash",
+    function()
+        if vim.g.nvdash_displayed then
+            vim.cmd "bd"
+        else
+            require("hydra_ui.dash").open(vim.api.nvim_create_buf(false, true))
+        end
+    end, {})
 
 if vim.g.hydravim_dash then
-  vim.defer_fn(function()
-    require("hydra_ui.dash").open()
-  end, 0)
+    vim.defer_fn(function()
+        require("hydra_ui.dash").open()
+    end, 0)
 end
