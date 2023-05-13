83228b5abd411ebcd0982576725fa5f712c4a401
siduck <siduck@tutanota.com>
2023-05-13 16:52:07 +0530

    tabufline: filter bufs before initializing | fix #63

--- lua/nvchad_ui/tabufline/lazyload.lua
+++ lua/nvchad_ui/tabufline/lazyload.lua
@@ -3,38 +3,43 @@ local opts = require("core.utils").load_config().ui.tabufline
 -- store listed buffers in tab local var
 vim.t.bufs = vim.api.nvim_list_bufs()
 
+local listed_bufs = {}
+
+for _, val in ipairs(vim.t.bufs) do
+  if vim.bo[val].buflisted then
+    table.insert(listed_bufs, val)
+  end
+end
+
+vim.t.bufs = listed_bufs
+
 -- autocmds for tabufline -> store bufnrs on bufadd, bufenter events
 -- thx to https://github.com/ii14 & stores buffer per tab -> table
 vim.api.nvim_create_autocmd({ "BufAdd", "BufEnter", "tabnew" }, {
   callback = function(args)
-    if vim.t.bufs == nil then
-      vim.t.bufs = vim.api.nvim_get_current_buf() == args.buf and {} or { args.buf }
-    else
-      local bufs = vim.t.bufs
+    local bufs = vim.t.bufs
 
-      -- check for duplicates
-      if
-        not vim.tbl_contains(bufs, args.buf)
-        and (args.event == "BufEnter" or vim.bo[args.buf].buflisted)
-        and (args.event == "BufEnter" or args.buf ~= vim.api.nvim_get_current_buf())
-        and vim.api.nvim_buf_is_valid(args.buf)
-        and vim.bo[args.buf].buflisted
-      then
-        table.insert(bufs, args.buf)
+    -- check for duplicates
+    if
+      not vim.tbl_contains(bufs, args.buf)
+      and (args.event == "BufEnter" or vim.bo[args.buf].buflisted or args.buf ~= vim.api.nvim_get_current_buf())
+      and vim.api.nvim_buf_is_valid(args.buf)
+      and vim.bo[args.buf].buflisted
+    then
+      table.insert(bufs, args.buf)
 
-        -- remove unnamed buffer which isnt current buf & modified
-        for index, bufnr in ipairs(bufs) do
-          if
-            #vim.api.nvim_buf_get_name(bufnr) == 0
-            and (vim.api.nvim_get_current_buf() ~= bufnr or bufs[index + 1])
-            and not vim.api.nvim_buf_get_option(bufnr, "modified")
-          then
-            table.remove(bufs, index)
-          end
+      -- remove unnamed buffer which isnt current buf & modified
+      for index, bufnr in ipairs(bufs) do
+        if
+          #vim.api.nvim_buf_get_name(bufnr) == 0
+          and (vim.api.nvim_get_current_buf() ~= bufnr or bufs[index + 1])
+          and not vim.api.nvim_buf_get_option(bufnr, "modified")
+        then
+          table.remove(bufs, index)
         end
-
-        vim.t.bufs = bufs
       end
+
+      vim.t.bufs = bufs
     end
   end,
 })
