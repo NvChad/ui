2d6ef336405fd82835e8ec1ec0611a8c9fa6616c
siduck <siduck@tutanota.com>
2023-05-16 07:13:35 +0530

    use nvim_tabpage_list_wins instead of nvim_list_wins to get largest win

--- lua/nvchad_ui/cheatsheet/init.lua
+++ lua/nvchad_ui/cheatsheet/init.lua
@@ -4,7 +4,7 @@ M.getLargestWin = function()
   local largest_win_width = 0
   local largest_win_id = 0
 
-  for _, winid in ipairs(vim.api.nvim_list_wins()) do
+  for _, winid in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
     local tmp_width = vim.api.nvim_win_get_width(winid)
 
     if tmp_width > largest_win_width then
