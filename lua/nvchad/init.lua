f50f9834e522b62ea89a84775091c1cf89af57a4
siduck <siduck@tutanota.com>
2023-11-24 18:56:03 +0530

    prevent nvdash showing on new file #212

--- lua/nvchad/init.lua
+++ lua/nvchad/init.lua
@@ -20,8 +20,10 @@ end, {})
 if config.nvdash.load_on_startup then
   vim.defer_fn(function()
     local buf_lines = vim.api.nvim_buf_get_lines(0, 0, 1, false)
+    local no_buf_content = vim.api.nvim_buf_line_count(0) == 1 and buf_lines[1] == ""
+    local bufname = vim.api.nvim_buf_get_name(0)
 
-    if (vim.api.nvim_buf_line_count(0) == 1) and buf_lines[1] == "" then
+    if bufname == "" and no_buf_content then
       require("nvchad.nvdash").open()
     end
   end, 0)
