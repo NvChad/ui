c67e3af14d79c3c03269474a5b9cda5fb2bf3a6e
siduck <siduck@tutanota.com>
2024-04-22 05:38:04 +0530

    misc

--- lua/nvchad/cheatsheet/init.lua
+++ lua/nvchad/cheatsheet/init.lua
@@ -11,7 +11,7 @@ M.create_fullsize_win = function(buf)
 end
 
 M.get_mappings = function(mappings, tb_to_add)
-  local excluded_groups = require("nvconfig").ui.cheatsheet.excluded_groups
+  local excluded_groups = require("nvconfig").ui.cheatsheet.excluded_groups or {}
 
   for _, v in ipairs(mappings) do
     local desc = v.desc
