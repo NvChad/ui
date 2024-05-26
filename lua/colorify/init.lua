41a261399832920b9976a20265ec572dbf88b559
siduck <siduck@tutanota.com>
2024-05-26 18:00:02 +0530

    colorify: delete extmarks only of those whose texts have been deleted!

--- lua/colorify/init.lua
+++ lua/colorify/init.lua
@@ -104,11 +104,11 @@ local function colorify(args)
 
   if args.event == "BufEnter" then
     api.nvim_buf_attach(args.buf, false, {
-      on_bytes = function(_, b, _, line, col)
-        local ms = get_extmarks(b, ns, { line, col }, { line, -1 }, { limit = 1 })
+      on_bytes = function(_, b, _, line, col, _, _, _, old_endcol)
+        local ms = get_extmarks(b, ns, { line, col }, { line, col + old_endcol }, { overlap = true })
 
-        if #ms > 0 then
-          api.nvim_buf_del_extmark(b, ns, ms[1][1])
+        for _, mark in ipairs(ms) do
+          api.nvim_buf_del_extmark(b, ns, mark[1])
         end
       end,
     })
