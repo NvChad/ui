63561ce533068af4e2ec19f31eca3674e4f4db19
siduck <siduck@tutanota.com>
2024-05-27 08:35:51 +0530

    colorify: handle lines that were deleted by on_lines

--- lua/colorify/init.lua
+++ lua/colorify/init.lua
@@ -1,3 +1,4 @@
+local fn = vim.fn
 local api = vim.api
 local ns = api.nvim_create_namespace "Colorify"
 
@@ -13,7 +14,6 @@ local get_extmarks = api.nvim_buf_get_extmarks
 local function highlight_hex(buf, line, str)
   for col, hex in str:gmatch "()(#%x%x%x%x%x%x)" do
     local hl_group = utils.add_hl(ns, hex)
-    local linenr = line - 1
     local end_col = col + #hex - 1
 
     local opts = { end_col = end_col, hl_group = hl_group }
@@ -24,13 +24,13 @@ local function highlight_hex(buf, line, str)
       opts.virt_text = { { conf.virt_txt, hl_group } }
     end
 
-    if needs_hl(buf, ns, linenr, col - 1, hl_group, opts) then
-      set_extmark(buf, ns, linenr, col - 1, opts)
+    if needs_hl(buf, ns, line, col - 1, hl_group, opts) then
+      set_extmark(buf, ns, line, col - 1, opts)
     end
   end
 end
 
-local function highlight_lspvars(buf, line, rows)
+local function highlight_lspvars(buf, line, min, max)
   local param = { textDocument = vim.lsp.util.make_text_document_params() }
 
   for _, client in pairs(vim.lsp.get_clients { bufnr = buf }) do
@@ -42,9 +42,9 @@ local function highlight_lspvars(buf, line, rows)
           end, resp)
         end
 
-        if resp and rows then
+        if resp and min then
           resp = vim.tbl_filter(function(v)
-            return v.range["start"].line >= rows.min and v.range["end"].line <= rows.max
+            return v.range["start"].line >= min and v.range["end"].line <= max
           end, resp)
         end
 
@@ -74,48 +74,27 @@ local function highlight_lspvars(buf, line, rows)
 end
 
 ------------------------------- main function ------------------------------------------------
-local function colorify(args)
+local function colorify_lines(args)
   local buf = args.buf
-  local line = vim.fn.line
   local winid = vim.fn.bufwinid(buf)
 
-  local rows = {
-    min = line("w0", winid) - 1,
-    max = line("w$", winid) + 1,
-  }
+  local min = fn.line("w0", winid) - 1
+  local max = fn.line("w$", winid) + 1
 
   if args.event == "TextChangedI" then
+    local cur_linenr = fn.line(".", winid) - 1
     local cur_line = api.nvim_get_current_line()
-    highlight_hex(buf, line ".", cur_line)
-    highlight_lspvars(buf, line "." - 1)
+    highlight_hex(buf, cur_linenr, cur_line)
+    highlight_lspvars(buf, cur_linenr)
   else
-    local lines = api.nvim_buf_get_lines(buf, rows.min, rows.max, false)
+    local lines = api.nvim_buf_get_lines(buf, min, max, false)
 
     for i, str in ipairs(lines) do
-      highlight_hex(buf, i, str)
+      highlight_hex(buf, i - 1, str)
     end
 
     if vim.bo[buf].buflisted then
-      highlight_lspvars(buf, nil, rows)
-    end
-  end
-
-  if args.event == "BufEnter" then
-    if not vim.b[buf].colorify_attached then
-      vim.b[buf].colorify_attached = true
-
-      api.nvim_buf_attach(args.buf, false, {
-        on_bytes = function(_, b, _, linenr, col, _, _, _, old_endcol)
-          local ms = get_extmarks(b, ns, { linenr, col }, { linenr, col + old_endcol }, { overlap = true })
-
-          for _, mark in ipairs(ms) do
-            api.nvim_buf_del_extmark(b, ns, mark[1])
-          end
-        end,
-        on_detach = function()
-          vim.b[buf].colorify_attached = false
-        end,
-      })
+      highlight_lspvars(buf, nil, min, max)
     end
   end
 end
@@ -127,7 +106,47 @@ api.nvim_create_autocmd({
   "VimResized",
   "LspAttach",
   "WinScrolled",
-  "BufEnter",
 }, {
-  callback = colorify,
+  callback = colorify_lines,
 })
+
+local handle_deletions = function(args)
+  local buf = args.buf
+
+  if vim.b[buf].colorify_attached then
+    return
+  end
+
+  vim.b[buf].colorify_attached = true
+
+  api.nvim_buf_attach(args.buf, false, {
+    -- del extmarks of all lines except the current
+    on_lines = function(_, b, _, first_line, last_line)
+      local ms = get_extmarks(b, ns, { first_line, 0 }, { last_line, 0 }, {})
+
+      for _, mark in ipairs(ms) do
+        if mark[2] ~= (fn.line "." - 1) then
+          api.nvim_buf_del_extmark(b, ns, mark[1])
+        end
+      end
+    end,
+
+    -- handle only current line extmarks
+    on_bytes = function(_, b, _, line, col, _, _, _, endcol)
+      if line ~= fn.line "." - 1 then
+        return
+      end
+
+      local ms = get_extmarks(b, ns, { line, col }, { line, col + endcol }, { overlap = true })
+
+      for _, mark in ipairs(ms) do
+        api.nvim_buf_del_extmark(b, ns, mark[1])
+      end
+    end,
+    on_detach = function()
+      vim.b[buf].colorify_attached = false
+    end,
+  })
+end
+
+api.nvim_create_autocmd("BufEnter", { callback = handle_deletions })
