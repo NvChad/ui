bfd0995b78376c342e84de580c85f842b163a75f
siduck <siduck@tutanota.com>
2024-07-22 09:50:37 +0530

    add minimal styled screen for urgent feedback situation
    https://github.com/NvChad/NvChad/issues/2938

---
+++ lua/nvchad_feedback.lua
@@ -0,0 +1,9 @@
+return function()
+  local dir = vim.fn.stdpath "data" .. "/nvchad_feedback"
+
+  if not vim.loop.fs_stat(dir) then
+    vim.fn.mkdir(dir, "p")
+    vim.cmd.bd()
+    vim.cmd "h nvchad_feedback | only"
+  end
+end
