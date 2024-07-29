4dd9c7c78ac6f2a7a269c71170cd0edafb9012c5
siduck <siduck@tutanota.com>
2024-07-29 09:31:32 +0530

    telescope themes: replace theme="name" instead of "name"

--- lua/telescope/_extensions/themes.lua
+++ lua/telescope/_extensions/themes.lua
@@ -64,13 +64,13 @@ local function switcher()
       actions.select_default:replace(function()
         if action_state.get_selected_entry() then
           local chadrc = dofile(vim.fn.stdpath "config" .. "/lua/chadrc.lua")
+
           local old_theme = chadrc.ui.theme or chadrc.base46.theme
+          old_theme = 'theme = "' .. old_theme .. '"'
 
-          old_theme = '"' .. old_theme .. '"'
+          local theme = 'theme = "' .. action_state.get_selected_entry()[1] .. '"'
 
-          local selected_theme = action_state.get_selected_entry()[1]
-
-          require("nvchad.utils").replace_word(old_theme, '"' .. selected_theme .. '"')
+          require("nvchad.utils").replace_word(old_theme, theme)
           actions.close(prompt_bufnr)
         end
       end)
