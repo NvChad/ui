6ebb19e314a1d672445efbca888ecb132ca365dd
siduck <siduck@tutanota.com>
2024-07-30 21:14:20 +0530

    add note about colorify.lighten func

--- lua/nvchad/colorify/init.lua
+++ lua/nvchad/colorify/init.lua
@@ -4,6 +4,7 @@ M.run = function()
   require "nvchad.colorify.run"
 end
 
+-- lightens hex color under cursor, negative arg will darken
 M.lighten = require("nvchad.colorify.tools").lighten
 
 return M
