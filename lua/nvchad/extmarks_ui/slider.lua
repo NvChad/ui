5d72ec0cabfe8569fc78cf68303a74ba60f68a40
siduck <siduck@tutanota.com>
2024-09-04 19:16:08 +0530

    perf(extmarks): make slider more accurate & just use 2 extmarks!

--- lua/nvchad/extmarks_ui/slider.lua
+++ lua/nvchad/extmarks_ui/slider.lua
@@ -1,44 +1,73 @@
-return function(o)
+local api = vim.api
+local M = {}
+
+M.val = function(w, left_txt, xpad, opts)
+  opts = opts or {}
+  local txt_len = vim.fn.strwidth(left_txt or "")
+  w = w - txt_len - (opts.ratio and 5 or 0)
+
+  local col = (api.nvim_win_get_cursor(0)[2] - txt_len - xpad)
+  col = opts.thumb and col + 1 or col
+
+  col = col >= w and w or col
+  col = col <= 0 and 0 or col
+
+  return math.ceil((col / w) * 100)
+end
+
+M.config = function(o)
   local line = {}
 
-  if o.thumb then
-    o.w = o.w - 1
-  end
+  local left_txt_len = vim.fn.strwidth(o.txt or "")
 
   if o.txt then
-    table.insert(line, { o.txt .. "  " })
-    o.w = o.w - vim.fn.strwidth(line[1][1])
+    table.insert(line, { o.txt })
+    o.w = o.w - left_txt_len
   end
+
+  o.w = (o.ratio_txt and o.w - 5) or o.w
+
+  local active_i = math.ceil((o.val / 100) * o.w)
+  local thumb_icon = ""
+
+  if o.thumb then
+    thumb_icon = o.thumb_icon or ""
+    thumb_icon = thumb_icon .. (o.val < 100 and " " or "")
+    thumb_icon = o.val == 0 and "" or thumb_icon
+  end
+
+  local active_str = string.rep("━", active_i - vim.fn.strwidth(thumb_icon))
+
+  local activemark = {
+    active_str .. thumb_icon,
+    o.hlon,
+    {
+      ui_type = "slider",
+      click = function()
+        o.actions()
+      end,
+    },
+  }
+
+  local inactivemark = {
+    string.rep("━", o.w - active_i),
+    o.hloff or "LineNr",
+    {
+      ui_type = "slider",
+      click = function()
+        o.actions()
+      end,
+    },
+  }
+
+  table.insert(line, activemark)
+  table.insert(line, inactivemark)
 
   if o.ratio_txt then
-    o.w = o.w - 5
-  end
-
-  local active_len = math.floor((o.val / 100) * o.w)
-  local ratio = o.ratio_txt and math.floor((active_len / o.w) * 100) or ""
-  local step = math.ceil(100 / o.w)
-
-  for i = 1, o.w, 1 do
-    local hlgroup = i <= active_len and o.hlon or (o.hloff or "LineNr")
-
-    local char = {
-      (o.thumb and i == active_len) and " " or "━",
-      -- "━",
-      hlgroup,
-      {
-        ui_type = "slider",
-        click = function()
-          o.actions(i * step)
-        end,
-      },
-    }
-
-    table.insert(line, char)
-  end
-
-  if o.ratio_txt then
-    table.insert(line, { "  " .. ratio .. " %", "Comment" })
+    table.insert(line, { "  " .. o.val .. "%", "Comment" })
   end
 
   return line
 end
+
+return M
