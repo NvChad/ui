dbdd2cfa7b6267e007e0b87ed7e2ea5c6979ef22
Thanh Tran <trongthanh@gmail.com>
2024-09-10 11:11:15 +0700

    feat(lsp): define default border style for LSP floating windows (#335)

--- lua/nvchad/lsp/init.lua
+++ lua/nvchad/lsp/init.lua
@@ -9,6 +9,14 @@ M.diagnostic_config = function()
     underline = true,
     float = { border = "single" },
   }
+
+  -- Default border style
+  local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+  function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+    opts = opts or {}
+    opts.border = "rounded"
+    return orig_util_open_floating_preview(contents, syntax, opts, ...)
+  end
 end
 
 return M
