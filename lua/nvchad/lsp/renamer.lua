f0697c32f9a7c42adacc873aaed8151c7863f506
siduck <siduck@tutanota.com>
2024-09-16 07:03:22 +0530

    chore(lsp.renamer): rm un-needed function

--- lua/nvchad/lsp/renamer.lua
+++ lua/nvchad/lsp/renamer.lua
@@ -1,17 +1,3 @@
-local map = vim.keymap.set
-
-local function apply(curr, win)
-  local newName = vim.trim(vim.fn.getline ".")
-  vim.api.nvim_win_close(win, true)
-
-  if #newName > 0 and newName ~= curr then
-    local params = vim.lsp.util.make_position_params()
-    params.newName = newName
-
-    vim.lsp.buf_request(0, "textDocument/rename", params)
-  end
-end
-
 return function()
   local currName = vim.fn.expand "<cword>" .. " "
 
@@ -32,10 +18,18 @@ return function()
   vim.cmd "normal A"
   vim.cmd "startinsert"
 
-  map({ "i", "n" }, "<Esc>", "<cmd>q<CR>", { buffer = 0 })
+  vim.keymap.set({ "i", "n" }, "<Esc>", "<cmd>q<CR>", { buffer = 0 })
 
-  map({ "i", "n" }, "<CR>", function()
-    apply(currName, win)
+  vim.keymap.set({ "i", "n" }, "<CR>", function()
+    local newName = vim.trim(vim.fn.getline ".")
+    vim.api.nvim_win_close(win, true)
+
+    if #newName > 0 and newName ~= currName then
+      local params = vim.lsp.util.make_position_params()
+      params.newName = newName
+
+      vim.lsp.buf_request(0, "textDocument/rename", params)
+    end
     vim.cmd.stopinsert()
   end, { buffer = 0 })
 end
