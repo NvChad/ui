bca9045cc4cf650bedab51dccddd60c7ab15a59a
siduck <siduck@tutanota.com>
2024-10-03 08:04:55 +0530

    chore(nvdash): use latest theme switcher instead of telescope one

--- lua/nvchad/au.lua
+++ lua/nvchad/au.lua
@@ -74,7 +74,3 @@ end, {})
 if config.colorify.enabled then
   require("nvchad.colorify").run()
 end
-
-if vim.version().minor < 10 then
-  vim.notify "Please update neovim version to v0.10 at least! NvChad only supports v0.10+"
-end
--- lua/nvconfig.lua
+++ lua/nvconfig.lua
@@ -62,7 +62,7 @@ local options = {
       { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
       { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
       { txt = "󰈭  Find Word", keys = "Spc f w", cmd = "Telescope live_grep" },
-      { txt = "󱥚  Themes", keys = "Spc t h", cmd = "Telescope themes" },
+      { txt = "󱥚  Themes", keys = "Spc t h", cmd = ":lua require('nvchad.themes').open()" },
       { txt = "  Mappings", keys = "Spc c h", cmd = "NvCheatsheet" },
 
       { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
