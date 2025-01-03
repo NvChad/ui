64893e35898c022a5ffc7842791c007e77694868
sheffey <57262511+SheffeyG@users.noreply.github.com>
2025-01-03 14:21:41 +0800

    fix(tabufline): correct buffer tab width

--- lua/nvchad/tabufline/utils.lua
+++ lua/nvchad/tabufline/utils.lua
@@ -62,7 +62,7 @@ M.style_buf = function(nr, i)
   end
 
   -- padding around bufname; 15= maxnamelen + 2 icon & space + 2 close icon
-  local pad = math.floor((23 - #name - 5) / 2)
+  local pad = math.floor((21 - #name - 5) / 2)
   pad = pad <= 0 and 1 or pad
 
   local maxname_len = 15
