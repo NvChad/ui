d5b72f5d31e0c53c3642d7ccc8e93ab639b0093a
= <=>
2025-01-07 06:33:54 +0900

    chore(stl): change spinner icons
    The old icons don't match the percentage visually, also adding a check
    circle at the end to indicate finished loading

--- lua/nvchad/stl/utils.lua
+++ lua/nvchad/stl/utils.lua
@@ -155,7 +155,7 @@ M.separators = {
 
 M.state = { lsp_msg = "" }
 
-local spinners = { "", "", "", "󰪞", "󰪟", "󰪠", "󰪢", "󰪣", "󰪤", "󰪥" }
+local spinners = { "", "󰪞", "󰪟", "󰪠", "󰪡", "󰪢", "󰪣", "󰪤", "󰪥", "" }
 
 M.autocmds = function()
   vim.api.nvim_create_autocmd("LspProgress", {
