d4f7e64892a24075d5fdd5a58bd0db45e86e38ed
anes <75726773+aneshodza@users.noreply.github.com>
2025-05-03 09:35:11 +0200

    fix(statusline): use vim.lsp.get_clients() instead of deprecated func()

--- lua/nvchad/statusline/default.lua
+++ lua/nvchad/statusline/default.lua
@@ -168,7 +168,7 @@ end
 
 M.LSP_status = function()
   if rawget(vim, "lsp") then
-    for _, client in ipairs(vim.lsp.get_active_clients()) do
+    for _, client in ipairs(vim.lsp.get_clients()) do
       if client.attached_buffers[stbufnr()] and client.name ~= "null-ls" then
         return (vim.o.columns > 100 and "%#St_LspStatus#" .. "   LSP ~ " .. client.name .. " ") or "   LSP "
       end
